49e53bd29e804815acfc192d726934a6b76442ea
Pokorson <pokorson@localhost.localdomain>
2019-10-27 19:56:07 +0100

    add merging and moving tiles from right to left

--- src/gameBoard.ts
+++ src/gameBoard.ts
@@ -36,6 +36,16 @@ export function createBoard(board?: number[][]): GameBoard {
     };
 }
 
+function serializeBoard(board: GameBoard): number[][] {
+    let serializedBoard = [[], [], [], []];
+    board.boardState.forEach((row, rowIndex) => {
+        row.forEach((tile, tileIndex) => {
+            serializedBoard[rowIndex][tileIndex] = tile.value;
+        })
+    })
+    return serializedBoard;
+}
+
 function isEmptyTile(tile) {
     return tile.value === 0;
 }
@@ -80,4 +90,57 @@ function canMoveColumns(board): boolean {
 
 export function boardHasPossibleMoves(board: GameBoard): boolean {
     return canMoveRows(board) || canMoveColumns(board);
+}
+
+function findLastAvailablePosition(row: number[], tileIndex: number): number {
+    for(let i = 0 ; i < tileIndex ; i++) {
+        if (row[i] === 0) {
+            return i;
+        }
+    }
+    
+    return tileIndex;
+}
+
+function shiftValuesToIndex(row: number[], index: number): number[] {
+    let resultRow = [...row];
+    return [...resultRow.slice(0, index), ...resultRow.slice(index + 1), 0];
+}
+
+function mergeValuesInRow(row: number[]): number[] {
+    let resultRow = [...row];
+    for(let i = 0; i < row.length ; i++) {
+        const value = resultRow[i];
+        const prevValue = resultRow[i - 1];
+
+        if(prevValue !== undefined && value !==0 && prevValue === value) {
+            resultRow[i - 1] = prevValue + value;
+            resultRow[i] = 0;
+            resultRow = shiftValuesToIndex(resultRow, i);
+        }
+    }
+    
+    return resultRow;
+}
+
+export function moveTiles(board: GameBoard): GameBoard {
+    let mergedBoard = serializeBoard(board);
+
+    mergedBoard.forEach((row, rowIndex) => {
+        row.forEach((value, tileIndex) => {
+            const prevTile = row[tileIndex - 1];
+            if (prevTile !== undefined && prevTile === 0) {
+                const lastAvailablePosition = findLastAvailablePosition(row, tileIndex);
+                mergedBoard[rowIndex][lastAvailablePosition] = value;
+                mergedBoard[rowIndex][tileIndex] = 0;
+            }
+             else {
+                mergedBoard[rowIndex][tileIndex] = value;
+            }
+        })
+    });
+
+    mergedBoard = mergedBoard.map(mergeValuesInRow);
+    
+    return createBoard(mergedBoard);
 }
--- tests/gameBoard.spec.ts
+++ tests/gameBoard.spec.ts
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {createBoard, boardHasPossibleMoves} from '../src/gameBoard';
+import {createBoard, boardHasPossibleMoves, moveTiles} from '../src/gameBoard';
 
 describe('gameBoard', function() {
     describe('#boardHasPossibleMoves', function() {
@@ -51,4 +51,162 @@ describe('gameBoard', function() {
             expect(boardHasPossibleMoves(board2)).to.be.true;
         })
     })
+
+    describe.only('#moveTiles', function() {
+        it('moves all tiles from right to left', function() {
+            const board = createBoard([
+                [0, 2, 8, 4],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            const expectedResult = createBoard([
+                [2, 8, 4, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            expect(moveTiles(board)).to.deep.equal(expectedResult);
+        })
+
+        it('move single tile to the last empty tile in row from right to left', function() {
+            const board = createBoard([
+                [0, 0, 0, 4],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            const expectedResult = createBoard([
+                [4, 0, 0, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            expect(moveTiles(board)).to.deep.equal(expectedResult);
+        });
+
+        it('moves all tiles to the last empty tile in row from right to left', function() {
+            const board = createBoard([
+                [0, 0, 4, 2],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            const expectedResult = createBoard([
+                [4, 2, 0, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            expect(moveTiles(board)).to.deep.equal(expectedResult);
+
+            const board2 = createBoard([
+                [0, 4, 2, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            const expectedResult2 = createBoard([
+                [4, 2, 0, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            expect(moveTiles(board2)).to.deep.equal(expectedResult2);
+        })
+
+        it('merges adjacent tiles when they have the same value', function() {
+            const board = createBoard([
+                [2, 2, 0, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            const expectedResult = createBoard([
+                [4, 0, 0, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            expect(moveTiles(board)).to.deep.equal(expectedResult);
+        })
+
+        it('merges adjacent tiles when they have the same value and place them at last available position', function() {
+            const board = createBoard([
+                [0, 2, 2, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            const expectedResult = createBoard([
+                [4, 0, 0, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            expect(moveTiles(board)).to.deep.equal(expectedResult);
+
+            const board2 = createBoard([
+                [2, 2, 2, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            const expectedResult2 = createBoard([
+                [4, 2, 0, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            expect(moveTiles(board2)).to.deep.equal(expectedResult2);
+        })
+
+        it('merges tiles with gap between them when they have the same value and place them at last available position', function() {
+            const board = createBoard([
+                [0, 2, 0, 2],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            const expectedResult = createBoard([
+                [4, 0, 0, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            expect(moveTiles(board)).to.deep.equal(expectedResult);
+        })
+
+        it('merges right tile to left one and place them at last available position', function() {
+            const board = createBoard([
+                [0, 2, 2, 2],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            const expectedResult = createBoard([
+                [4, 2, 0, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            expect(moveTiles(board)).to.deep.equal(expectedResult);
+        })
+
+        it('merges all tile pairs with the same value and place them at last available position', function() {
+            const board = createBoard([
+                [2, 2, 2, 2],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            const expectedResult = createBoard([
+                [4, 4, 0, 0],
+                [4, 2, 4, 8],
+                [4, 8, 2, 4],
+                [4, 2, 4, 8],
+            ]);
+            expect(moveTiles(board)).to.deep.equal(expectedResult);
+        })
+    });
 })
