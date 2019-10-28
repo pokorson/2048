0a2790403082140cbb2e11e5c1cb5e546949342b
Pokorson <pokorson@localhost.localdomain>
2019-10-28 09:42:59 +0100

    add generating new tile after each move

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { renderGameBoard, createBoard, moveTiles } from './src/gameBoard';
+import { renderGameBoard, createBoard, moveTiles, addNewTile } from './src/gameBoard';
 
 let board = createBoard([
     [4, 4, 8, 2],
@@ -6,25 +6,21 @@ let board = createBoard([
     [2, 4, 8, 2],
     [4, 2, 4, 8],
 ]);
+
 renderGameBoard(board, 'game-board');
 
 document.addEventListener('keyup', (event) => {
-    switch (event.key) {
-        case 'ArrowDown':
-            board = moveTiles(board, 'down');
-            renderGameBoard(board, 'game-board');
-            return;
-        case 'ArrowUp':
-            board = moveTiles(board, 'up');
-            renderGameBoard(board, 'game-board');
-            return;
-        case 'ArrowLeft':
-            board = moveTiles(board, 'left');
-            renderGameBoard(board, 'game-board');
-            return;
-        case 'ArrowRight':
-            board = moveTiles(board, 'right');
-            renderGameBoard(board, 'game-board');
-            return;
-    }
+    const arrowKeyDirectionMap = {
+        'ArrowDown': 'down',
+        'ArrowUp': 'up',
+        'ArrowLeft': 'left',
+        'ArrowRight': 'right'
+    };
+    if (!arrowKeyDirectionMap[event.key]) return;
+
+    board = moveTiles(board, arrowKeyDirectionMap[event.key]);
+
+    board = addNewTile(board);
+
+    renderGameBoard(board, 'game-board');
 })
